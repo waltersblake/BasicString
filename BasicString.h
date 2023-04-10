1461a1476993a72394d5316a47274b9035a26075
Blake Walters <waltersblake@protonmail.com>
2023-04-11 07:47:22 +1000

    formatting

--- BasicString.h
+++ BasicString.h
@@ -7,15 +7,15 @@ class BasicString {
 private:
   size_t max_size;
   size_t length;
-  char *buffer;
+  char* buffer;
 
 public:
   BasicString(size_t max_size);
   ~BasicString();
-  BasicString(const BasicString &other);
-  BasicString(const BasicString &&other);
-  BasicString &operator=(const BasicString &other);
-  BasicString &operator=(const BasicString &&other);
-  bool append_line(char *line);
+  BasicString(const BasicString& other);
+  BasicString(const BasicString&& other);
+  BasicString& operator=(const BasicString& other);
+  BasicString& operator=(const BasicString&& other);
+  bool append_line(char* line);
 };
 } // namespace basicstring
