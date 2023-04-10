3d74eae79729b9fe7241263bd63b1b84247083b1
Blake Walters <waltersblake@protonmail.com>
2023-04-11 07:31:33 +1000

    Declare BasicString class

---
+++ BasicString.h
@@ -0,0 +1,21 @@
+#include <cstddef>
+#include <cstring>
+
+namespace basicstring {
+
+class BasicString {
+private:
+  size_t max_size;
+  size_t length;
+  char *buffer;
+
+public:
+  BasicString(size_t max_size);
+  ~BasicString();
+  BasicString(const BasicString &other);
+  BasicString(const BasicString &&other);
+  BasicString &operator=(const BasicString &other);
+  BasicString &operator=(const BasicString &&other);
+  bool append_line(char *line);
+};
+} // namespace basicstring
