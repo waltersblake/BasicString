257aefa2a35bb581011a826474d752cfec705242
Blake Walters <waltersblake@protonmail.com>
2023-04-11 08:58:08 +1000

    Initial definition

---
+++ BasicString.cpp
@@ -0,0 +1,77 @@
+#include "BasicString.h"
+#include <cstddef>
+#include <cstring>
+#include <stdexcept>
+#include <utility>
+
+namespace basicstring {
+
+BasicString::BasicString(size_t max_size) : max_size{max_size}, length{} {
+  if (max_size == 0) {
+    throw std::runtime_error("Max size must be greater than 0");
+  }
+  buffer = new char[max_size];
+  memset(buffer, 0, max_size);
+}
+
+BasicString::~BasicString() { delete[] buffer; }
+
+// Copy constructor
+BasicString::BasicString(const BasicString& other)
+    : max_size{other.max_size}, length{other.length} {
+  buffer = new char[max_size];
+  std::strncpy(buffer, other.buffer, length);
+}
+
+// Move constructor
+BasicString::BasicString(BasicString&& other) noexcept
+    : max_size{other.max_size}, length{other.length}, buffer{std::move(other.buffer)} {
+  other.length = 0;
+  other.max_size = 0;
+}
+
+// Copy assignment
+BasicString& BasicString::operator=(const BasicString& other) {
+  if (this == &other) {
+    return *this;
+  }
+  delete[] buffer;
+  max_size = other.max_size;
+  length = other.length;
+  buffer = new char[max_size];
+  memset(buffer, 0, max_size);
+  std::strncpy(buffer, other.buffer, length);
+
+  return *this;
+}
+
+// Move assignment
+BasicString& BasicString::operator=(BasicString&& other) {
+  if (this == &other) {
+    return *this;
+  }
+  max_size = other.max_size;
+  length = other.length;
+  buffer = std::move(other.buffer);
+
+  other.max_size = 0;
+  other.length = 0;
+
+  return *this;
+}
+
+bool BasicString::append_line(const char* line) {
+  size_t line_length = strlen(line);
+  if (line_length + length + 2 > max_size) {
+    return false;
+  }
+  std::strncpy(buffer + length, line, max_size - length);
+  length += line_length;
+  buffer[length++] = '\n';
+  buffer[length] = 0;
+  return true;
+}
+
+void BasicString::print(const char* prefix) const { printf("%s: %s", prefix, buffer); }
+
+} // namespace basicstring
